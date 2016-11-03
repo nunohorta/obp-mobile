2e4e313a99913a440a8e67b3f86290fb2a5cfd13
Nuno Horta <nunobhorta@gmail.com>
2016-11-03 23:20:52 +0000

    Constants to be used to authenticate

---
+++ Core/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace Core
+{
+	public class Constants
+	{
+		public static string ClientId = "";
+		public static string ClientSecret = "";
+
+		public static string Scope = "";
+		public static string AuthorizeUrl = "https://api.openbankproject.com/oauth/initiate";
+		public static string AccessTokenUrl = "";
+		public static string UserInfoUrl = "";
+		public static string RedirectUrl = "https://api.openbankproject.com/oauth/authorize";
+	}
+}
